a976a946d16748edf320aab5dec748c1cdb375b4
Md Shoharab Pk <Md Shoharab Pk>
2021-01-31 23:16:35 +0600

    template complate

---
+++ template.js
@@ -0,0 +1,18 @@
+const firstName = "Shoharab";
+const lastName = "Pk";
+const fullName = firstName + " " + lastName + " is a bad boy";
+console.log(fullName);
+
+const fullName2 = `${firstName} ${25+50+45/5*9} is a good boy`;
+console.log(fullName2);
+
+const multiline = "i love you\n"
+    + "i miss you\n"
+    + "i need you";
+    console.log(multiline);
+
+    const multiline2 = `i love you
+  i miss you
+    i don't need you
+    `;
+    console.log(multiline2);
